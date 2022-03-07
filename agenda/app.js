ac6fc5090e759ad87667a7e9e532e3ff613fb2d7
lucianoeun <lmideias@gmail.com>
2022-03-07 19:56:37 -0300

    criaçao da agenda json

--- agenda/app.js
+++ agenda/app.js
@@ -1,104 +1,75 @@
 var app = new Vue({
     el: '#app',
     data: {
-        meses: 'janeiro',
+        meses: 'Março',
         semanas: 1,
-        calendario: [{
-                id: 1,
-                datas: '02/01/2022',
-                mes: 'janeiro',
-                numero: 1,
-                semana: 'segunda',
-                dia: 02
-            },
-            {
-                id: 2,
-                datas: '03/01/2022',
-                mes: 'janeiro',
-                numero: 1,
-                semana: 'terça',
-                dia: 03
-            },
-            {
-                id: 3,
-                datas: '04/01/2022',
-                mes: 'janeiro',
-                numero: 1,
-                semana: 'quarta',
-                dia: 04
-            },
-            {
-                id: 4,
-                datas: '05/01/2022',
-                mes: 'janeiro',
-                numero: 1,
-                semana: 'quinta',
-                dia: 05
-            },
-            {
-                id: 5,
-                datas: '08/01/2022',
-                mes: 'janeiro',
-                numero: 2,
-                semana: 'segunda',
-                dia: 08
-            },
-            {
-                id: 6,
-                datas: '09/01/2022',
-                mes: 'janeiro',
-                numero: 2,
-                semana: 'terça',
-                dia: 09
-            },
-            {
-                id: 7,
-                datas: '10/01/2022',
-                mes: 'janeiro',
-                numero: 2,
-                semana: 'quarta',
-                dia: 10
-            },
-            {
-                id: 8,
-                datas: '11/01/2022',
-                mes: 'janeiro',
-                numero: 2,
-                semana: 'quinta',
-                dia: 11
-            },
-            {
-                id: 9,
-                datas: '01/02/2022',
-                mes: 'fevereiro',
-                numero: 1,
-                semana: 'segunda',
-                dia: 01
-            },
-            {
-                id: 10,
-                datas: '02/02/2022',
-                mes: 'fevereiro',
-                numero: 1,
-                semana: 'terça',
-                dia: 02
-            },
-            {
-                id: 11,
-                datas: '03/02/2022',
-                mes: 'fevereiro',
-                numero: 1,
-                semana: 'quarta',
-                dia: 03
-            },
-            {
-                id: 12,
-                datas: '04/02/2022',
-                mes: 'fevereiro',
-                numero: 1,
-                semana: 'quinta',
-                dia: 04
-            }
+        calendario: [
+            { datas: '01/03/2022', mes:'Março', semanas: 1, semana: 'Terça-feira', dia: 03 },
+            { datas: '02/03/2022', mes:'Março', semanas: 1, semana: 'Quarta-feira', dia: 04 },
+            { datas: '03/03/2022', mes:'Março', semanas: 1, semana: 'Quinta-feira', dia: 05 },
+
+            { datas: '07/03/2022', mes:'Março', semanas: 2, semana: 'Segunda-feira', dia: 07 },
+            { datas: '08/03/2022', mes:'Março', semanas: 2, semana: 'Terça-feira', dia: 08 },
+            { datas: '09/03/2022', mes:'Março', semanas: 2, semana: 'Quarta-feira', dia: 09 },
+            { datas: '10/03/2022', mes:'Março', semanas: 2, semana: 'Quinta-feira', dia: 10 },
+
+            { datas: '14/03/2022', mes:'Março', semanas: 3, semana: 'Segunda-feira', dia: 14 },
+            { datas: '15/03/2022', mes:'Março', semanas: 3, semana: 'Terça-feira', dia: 15 },
+            { datas: '16/03/2022', mes:'Março', semanas: 3, semana: 'Quarta-feira', dia: 16 },
+            { datas: '17/03/2022', mes:'Março', semanas: 3, semana: 'Quinta-feira', dia: 17 },
+
+            { datas: '21/03/2022', mes:'Março', semanas: 4, semana: 'Segunda-feira', dia: 21 },
+            { datas: '22/03/2022', mes:'Março', semanas: 4, semana: 'Terça-feira', dia: 22 },
+            { datas: '23/03/2022', mes:'Março', semanas: 4, semana: 'Quarta-feira', dia: 23 },
+            { datas: '24/03/2022', mes:'Março', semanas: 4, semana: 'Quinta-feira', dia: 24 },
+
+            { datas: '28/03/2022', mes:'Março', semanas: 5, semana: 'Segunda-feira', dia: 28 },
+            { datas: '29/03/2022', mes:'Março', semanas: 5, semana: 'Terça-feira', dia: 29 },
+            { datas: '30/03/2022', mes:'Março', semanas: 5, semana: 'Quarta-feira', dia: 30 },
+            { datas: '31/03/2022', mes:'Março', semanas: 5, semana: 'Quinta-feira', dia: 31 },
+
+            { datas: '04/04/2022', mes:'Abril', semanas: 1, semana: 'Segunda-feira', dia: 04 },
+            { datas: '05/04/2022', mes:'Abril', semanas: 1, semana: 'Terça-feira', dia: 05 },
+            { datas: '06/04/2022', mes:'Abril', semanas: 1, semana: 'Quarta-feira', dia: 06 },
+            { datas: '07/04/2022', mes:'Abril', semanas: 1, semana: 'Quinta-feira', dia: 07 },
+
+            { datas: '11/04/2022', mes:'Abril', semanas: 2, semana: 'Segunda-feira', dia: 11 },
+            { datas: '12/04/2022', mes:'Abril', semanas: 2, semana: 'Terça-feira', dia: 12 },
+            { datas: '13/04/2022', mes:'Abril', semanas: 2, semana: 'Quarta-feira', dia: 13 },
+            { datas: '14/04/2022', mes:'Abril', semanas: 2, semana: 'Quinta-feira', dia: 14 },
+
+            { datas: '18/04/2022', mes:'Abril', semanas: 3, semana: 'Segunda-feira', dia: 18 },
+            { datas: '19/04/2022', mes:'Abril', semanas: 3, semana: 'Terça-feira', dia: 19 },
+            { datas: '20/04/2022', mes:'Abril', semanas: 3, semana: 'Quarta-feira', dia: 20 },
+            { datas: '21/04/2022', mes:'Abril', semanas: 3, semana: 'Quinta-feira', dia: 21 },
+
+            { datas: '25/04/2022', mes:'Abril', semanas: 4, semana: 'Segunda-feira', dia: 25 },
+            { datas: '26/04/2022', mes:'Abril', semanas: 4, semana: 'Terça-feira', dia: 26 },
+            { datas: '27/04/2022', mes:'Abril', semanas: 4, semana: 'Quarta-feira', dia: 27 },
+            { datas: '28/04/2022', mes:'Abril', semanas: 4, semana: 'Quinta-feira', dia: 28 }, 
+
+            { datas: '02/05/2022', mes:'Maio', semanas: 1, semana: 'Segunda-feira', dia: 02 },
+            { datas: '03/05/2022', mes:'Maio', semanas: 1, semana: 'Terça-feira', dia: 03 },
+            { datas: '04/05/2022', mes:'Maio', semanas: 1, semana: 'Quarta-feira', dia: 04 },
+            { datas: '05/05/2022', mes:'Maio', semanas: 1, semana: 'Quinta-feira', dia: 05 },
+
+            { datas: '09/05/2022', mes:'Maio', semanas: 2, semana: 'Segunda-feira', dia: 09 },
+            { datas: '10/05/2022', mes:'Maio', semanas: 2, semana: 'Terça-feira', dia: 10 },
+            { datas: '11/05/2022', mes:'Maio', semanas: 2, semana: 'Quarta-feira', dia: 11 },
+            { datas: '12/05/2022', mes:'Maio', semanas: 2, semana: 'Quinta-feira', dia: 12 },
+
+            { datas: '16/05/2022', mes:'Maio', semanas: 3, semana: 'Segunda-feira', dia: 16 },
+            { datas: '17/05/2022', mes:'Maio', semanas: 3, semana: 'Terça-feira', dia: 17 },
+            { datas: '18/05/2022', mes:'Maio', semanas: 3, semana: 'Quarta-feira', dia: 18 },
+            { datas: '19/05/2022', mes:'Maio', semanas: 3, semana: 'Quinta-feira', dia: 19 },
+
+            { datas: '23/05/2022', mes:'Maio', semanas: 4, semana: 'Segunda-feira', dia: 23 },
+            { datas: '24/05/2022', mes:'Maio', semanas: 4, semana: 'Terça-feira', dia: 24 },
+            { datas: '25/05/2022', mes:'Maio', semanas: 4, semana: 'Quarta-feira', dia: 25 },
+            { datas: '26/05/2022', mes:'Maio', semanas: 4, semana: 'Quinta-feira', dia: 26 },  
+            
+            { datas: '30/05/2022', mes:'Maio', semanas: 5, semana: 'Segunda-feira', dia: 30 },
+            { datas: '31/05/2022', mes:'Maio', semanas: 5, semana: 'Terça-feira', dia: 31 }
         ]
     },
     methods: {
--- agenda/calendario.js
+++ agenda/calendario.js
@@ -1,30 +1,83 @@
-const calendario = [{
-        id: 1,
-        datas: '02/01/2022',
-        mes: 'janeiro',
-        numero: 1,
-        semana: 'segunda',
-        dia: 01
-    },
-    {
-        id: 2,
-        datas: '03/01/2022',
-        mes: 'janeiro',
-        numero: 1,
-        semana: 'terça',
-        dia: 02
-    },
-    {
-        id: 3,
-        datas: '01/02/2022',
-        mes: 'fevereiro',
-        numero: 1,
-        semana: 'segunda',
-        dia: 01
-    }
+const marco = [
+    { datas: '01/03/2022', mes:'Março', semanas: 1, semana: 'Terça-feira', dia: 03 },
+    { datas: '02/03/2022', mes:'Março', semanas: 1, semana: 'Quarta-feira', dia: 04 },
+    { datas: '03/03/2022', mes:'Março', semanas: 1, semana: 'Quinta-feira', dia: 05 },
+
+    { datas: '07/03/2022', mes:'Março', semanas: 2, semana: 'Segunda-feira', dia: 07 },
+    { datas: '08/03/2022', mes:'Março', semanas: 2, semana: 'Terça-feira', dia: 08 },
+    { datas: '09/03/2022', mes:'Março', semanas: 2, semana: 'Quarta-feira', dia: 09 },
+    { datas: '10/03/2022', mes:'Março', semanas: 2, semana: 'Quinta-feira', dia: 10 },
+
+    { datas: '14/03/2022', mes:'Março', semanas: 3, semana: 'Segunda-feira', dia: 14 },
+    { datas: '15/03/2022', mes:'Março', semanas: 3, semana: 'Terça-feira', dia: 15 },
+    { datas: '16/03/2022', mes:'Março', semanas: 3, semana: 'Quarta-feira', dia: 16 },
+    { datas: '17/03/2022', mes:'Março', semanas: 3, semana: 'Quinta-feira', dia: 17 },
+
+    { datas: '21/03/2022', mes:'Março', semanas: 4, semana: 'Segunda-feira', dia: 21 },
+    { datas: '22/03/2022', mes:'Março', semanas: 4, semana: 'Terça-feira', dia: 22 },
+    { datas: '23/03/2022', mes:'Março', semanas: 4, semana: 'Quarta-feira', dia: 23 },
+    { datas: '24/03/2022', mes:'Março', semanas: 4, semana: 'Quinta-feira', dia: 24 },
+
+    { datas: '28/03/2022', mes:'Março', semanas: 5, semana: 'Segunda-feira', dia: 28 },
+    { datas: '29/03/2022', mes:'Março', semanas: 5, semana: 'Terça-feira', dia: 29 },
+    { datas: '30/03/2022', mes:'Março', semanas: 5, semana: 'Quarta-feira', dia: 30 },
+    { datas: '31/03/2022', mes:'Março', semanas: 5, semana: 'Quinta-feira', dia: 31 },
+   
 ];
 
-const resultado = calendario.filter(item => item.mes === 'janeiro');
-const rest = resultado.filter(item => item.numero === 01);
+const abril = [
+   
+    { datas: '04/04/2022', mes:'Abril', semanas: 1, semana: 'Segunda-feira', dia: 04 },
+    { datas: '05/04/2022', mes:'Abril', semanas: 1, semana: 'Terça-feira', dia: 05 },
+    { datas: '06/04/2022', mes:'Abril', semanas: 1, semana: 'Quarta-feira', dia: 06 },
+    { datas: '07/04/2022', mes:'Abril', semanas: 1, semana: 'Quinta-feira', dia: 07 },
 
-console.log(rest);
+    { datas: '11/04/2022', mes:'Abril', semanas: 2, semana: 'Segunda-feira', dia: 11 },
+    { datas: '12/04/2022', mes:'Abril', semanas: 2, semana: 'Terça-feira', dia: 12 },
+    { datas: '13/04/2022', mes:'Abril', semanas: 2, semana: 'Quarta-feira', dia: 13 },
+    { datas: '14/04/2022', mes:'Abril', semanas: 2, semana: 'Quinta-feira', dia: 14 },
+
+    { datas: '18/04/2022', mes:'Abril', semanas: 3, semana: 'Segunda-feira', dia: 18 },
+    { datas: '19/04/2022', mes:'Abril', semanas: 3, semana: 'Terça-feira', dia: 19 },
+    { datas: '20/04/2022', mes:'Abril', semanas: 3, semana: 'Quarta-feira', dia: 20 },
+    { datas: '21/04/2022', mes:'Abril', semanas: 3, semana: 'Quinta-feira', dia: 21 },
+
+    { datas: '25/04/2022', mes:'Abril', semanas: 4, semana: 'Segunda-feira', dia: 25 },
+    { datas: '26/04/2022', mes:'Abril', semanas: 4, semana: 'Terça-feira', dia: 26 },
+    { datas: '27/04/2022', mes:'Abril', semanas: 4, semana: 'Quarta-feira', dia: 27 },
+    { datas: '28/04/2022', mes:'Abril', semanas: 4, semana: 'Quinta-feira', dia: 28 },   
+   
+];
+
+
+const maio = [
+   
+    { datas: '02/05/2022', mes:'Maio', semanas: 1, semana: 'Segunda-feira', dia: 02 },
+    { datas: '03/05/2022', mes:'Maio', semanas: 1, semana: 'Terça-feira', dia: 03 },
+    { datas: '04/05/2022', mes:'Maio', semanas: 1, semana: 'Quarta-feira', dia: 04 },
+    { datas: '05/05/2022', mes:'Maio', semanas: 1, semana: 'Quinta-feira', dia: 05 },
+
+    { datas: '09/05/2022', mes:'Maio', semanas: 2, semana: 'Segunda-feira', dia: 09 },
+    { datas: '10/05/2022', mes:'Maio', semanas: 2, semana: 'Terça-feira', dia: 10 },
+    { datas: '11/05/2022', mes:'Maio', semanas: 2, semana: 'Quarta-feira', dia: 11 },
+    { datas: '12/05/2022', mes:'Maio', semanas: 2, semana: 'Quinta-feira', dia: 12 },
+
+    { datas: '16/05/2022', mes:'Maio', semanas: 3, semana: 'Segunda-feira', dia: 16 },
+    { datas: '17/05/2022', mes:'Maio', semanas: 3, semana: 'Terça-feira', dia: 17 },
+    { datas: '18/05/2022', mes:'Maio', semanas: 3, semana: 'Quarta-feira', dia: 18 },
+    { datas: '19/05/2022', mes:'Maio', semanas: 3, semana: 'Quinta-feira', dia: 19 },
+
+    { datas: '23/05/2022', mes:'Maio', semanas: 4, semana: 'Segunda-feira', dia: 23 },
+    { datas: '24/05/2022', mes:'Maio', semanas: 4, semana: 'Terça-feira', dia: 24 },
+    { datas: '25/05/2022', mes:'Maio', semanas: 4, semana: 'Quarta-feira', dia: 25 },
+    { datas: '26/05/2022', mes:'Maio', semanas: 4, semana: 'Quinta-feira', dia: 26 },  
+    
+    { datas: '30/05/2022', mes:'Maio', semanas: 5, semana: 'Segunda-feira', dia: 30 },
+    { datas: '31/05/2022', mes:'Maio', semanas: 5, semana: 'Terça-feira', dia: 31 },
+   
+];
+
+// const resultado = calendario.filter(item => item.mes === 'janeiro');
+// const rest = resultado.filter(item => item.numero === 01);
+
+// console.log(rest);
