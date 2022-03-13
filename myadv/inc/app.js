293d816d974ec18855ca1274a92206c51bcd4c15
lucianoeun <lmideias@gmail.com>
2022-03-13 20:15:58 -0300

    melhorias

--- myadv/inc/app.js
+++ myadv/inc/app.js
@@ -1,9 +1,20 @@
+const form = new Vue({
+    el: "#form",   
+    data: {
+       lista:[
+           {datas:'22/02/2022', titulo:'Pedra bonita feia', dono:'lucianoeun'},
+           {datas:'22/02/2022', titulo:'Pedra bonita feia', dono:'lucianoeun'},
+           {datas:'22/02/2022', titulo:'Pedra bonita feia', dono:'lucianoeun'},
+           {datas:'22/02/2022', titulo:'Pedra bonita feia', dono:'lucianoeun'},
+           {datas:'22/02/2022', titulo:'Pedra bonita feia', dono:'lucianoeun'}
+       ]
+    }
+});
+
 const app = new Vue({
     el: "#app",   
-    data: {
-       
+    data: {       
     },
-    methods: {
-        
+    methods: {        
     }
 });
