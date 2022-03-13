ee1ffa4c58983aa133d3ed716686bb531d3126c0
lucianoeun <lmideias@gmail.com>
2022-03-13 13:43:17 -0300

    modificações no jquery

--- ui/app.js
+++ ui/app.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
-    alert('Olá mundo');
+// $(document).ready(function() {
+//     alert('Olá mundo');
+// });
+
+
+$(function() {
+    
+    $('#boxa').fadeOut(1000).delay(1000).slideDown(1000);
+
+    $('#boxb').click(function() {
+        $(this).css('background-color', 'red');
+    })
+
+
 });
