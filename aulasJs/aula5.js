2ff2b670d239046bf5340435dcf58e05f5942c29
lucianoeun <lmideias@gmail.com>
2022-03-14 20:24:03 -0300

    novo sistema de agenda

---
+++ aulasJs/aula5.js
@@ -0,0 +1,8 @@
+const resultado = ()=>{
+    var n = 4;
+    var p = 6;
+    return n*p;
+   
+}
+
+console.log(resultado);
---
+++ eventos/app.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+    el: "#app",   
+    data: {
+       lista:[
+           {datas:'22/02/2022', titulo:'Pedra bonita feia', dono:'lucianoeun'},
+           {datas:'22/02/2022', titulo:'Pedra bonita feia', dono:'lucianoeun'},
+           {datas:'22/02/2022', titulo:'Pedra bonita feia', dono:'lucianoeun'},
+           {datas:'22/02/2022', titulo:'Pedra bonita feia', dono:'lucianoeun'},
+           {datas:'22/02/2022', titulo:'Pedra bonita feia', dono:'lucianoeun'}
+       ]
+    }
+});
